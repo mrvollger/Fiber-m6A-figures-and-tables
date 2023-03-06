ee8ce0dc0c380d3862c8e28d929e5de6f4ba0530
mrvollger <mrvollger@gmail.com>
2023-03-06 12:21:41 -0800

    updates for splitting by ml values

--- scripts/Motif_m6a_check.py
+++ scripts/Motif_m6a_check.py
@@ -21,9 +21,7 @@ def parse():
     parser = argparse.ArgumentParser(
         description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument(
-        "tbl", nargs="+", help="fibertools-rs all table"
-    )
+    parser.add_argument("tbl", nargs="+", help="fibertools-rs all table")
     parser.add_argument("-n", help="n rows to read", default=None, type=int)
     parser.add_argument("-b", help="buffer", default=0, type=int)
     parser.add_argument("-t", help="threads", default=8, type=int)
@@ -38,7 +36,7 @@ def parse():
     return args
 
 
-def get_stats(row, buffer=0, motifs=None):
+def get_stats(row, buffer=0, motifs=None, by_ml=True):
     seq = np.frombuffer(bytes(row.fiber_sequence, "utf-8"), dtype="S1")
     mask = np.zeros(seq.shape)
     # for motif in motifs:
@@ -46,7 +44,7 @@ def get_stats(row, buffer=0, motifs=None):
     # search = "|".join(["(?=" + motif + ")" for motif in motifs])
     search = "|".join(motifs)
     if motifs[0] == "All-AT-bp":
-        mask[0:mask.shape[0]] = 1
+        mask[0 : mask.shape[0]] = 1
     else:
         for m in re.finditer(search, row.fiber_sequence):
             s = m.start()
@@ -65,22 +63,36 @@ def get_stats(row, buffer=0, motifs=None):
         # return None
     else:
         m6a = np.array(row.m6a)
-        m6a_qual = np.array(row.m5a_qual)
+        m6a_qual = np.fromstring(row.m6a_qual, sep=",", dtype=np.int32)
+    # iterate over different ml values for on target rates
+    by_ml = {"on_target": [], "off_target": [], "min_ml": []}
+    ml_targets = np.unique(m6a_qual)
+    if not by_ml or len(ml_targets) == 0:
+        ml_targets = [0]
+    for min_ml in ml_targets:
+        use_m6a = m6a[m6a_qual >= min_ml]
+        by_ml["on_target"].append((mask[use_m6a] == 1).sum())
+        by_ml["off_target"].append((mask[use_m6a] == 0).sum())
+        by_ml["min_ml"].append(min_ml)
 
+    o_df = pd.DataFrame(by_ml)
+    # print(o_df)
+    # exit
     is_at = (seq == b"A") | (seq == b"T")
-    # set motif values not AT to zero
+    # Set motif values not AT to zero
     mask[~is_at] = 0
-    row["on_target"] = (mask[m6a] == 1).sum()
-    row["off_target"] = (mask[m6a] == 0).sum()
-    row["total_targets"] = mask.sum()
-    # row["motif_at_count"] = motif_at_count
-    # row["at_count"] = at_count
-    row["motif_at_count"] = (seq[mask == 1] == b"A").sum() + (
+    # total AT count
+    o_df["at_count"] = (seq == b"A").sum() + (seq == b"T").sum()
+    # targets in the read
+    o_df["total_targets"] = mask.sum()
+    # ATs in motif
+    o_df["motif_at_count"] = (seq[mask == 1] == b"A").sum() + (
         seq[mask == 1] == b"T"
     ).sum()
-    row["at_count"] = (seq == b"A").sum() + (seq == b"T").sum()
-    #sys.stderr.write(f"\r{row.st}")
-    return row
+
+    # row["on_target"] = (mask[m6a] == 1).sum()
+    # row["off_target"] = (mask[m6a] == 0).sum()
+    return o_df
 
 
 def by_table(inputs, buffer=0, n_rows=None):
@@ -91,44 +103,55 @@ def by_table(inputs, buffer=0, n_rows=None):
     # ddata = dd.from_pandas(fd, npartitions=30)
     helper_fun = partial(get_stats, buffer=buffer, motifs=m)
     results = fd.apply(helper_fun, axis=1)
+    results = pd.concat(list(results), axis=0)
+    # for x in results:
+    #    print(x)
+    # print(results)
     # results = ddata.map_partitions(
     #    lambda df: df.apply(helper_fun, axis=1)
     # ).compute()
 
-    on_target = results.on_target.sum()
-    off_target = results.off_target.sum()
-    motif_at_count = results.motif_at_count.sum()
-    at_count = results.at_count.sum()
-    total_targets = results.total_targets.sum()
-    on_target_rate = on_target / (on_target + off_target)
-    
-    rtn = (f"{os.path.basename(tbl)}"
-        + f"\t{';'.join(m)}"
-        + f"\t{on_target_rate:.3%}"
-        + f"\t{on_target/total_targets:.3%}"
-        + f"\t{motif_at_count / at_count:.3%}"
-        + f"\t{total_targets:,}"
-        + f"\t{fd.total_m6a_bp.sum()/fd.total_AT_bp.sum():.3%}"
-        + f"\t{fd.total_m6a_bp.sum()}\n")
+    rtn = ""
+    for min_ml in np.unique(results.min_ml):
+        cur_results = results[results.min_ml >= min_ml]
+        on_target = cur_results.on_target.sum()
+        off_target = cur_results.off_target.sum()
+        motif_at_count = cur_results.motif_at_count.sum()
+        at_count = cur_results.at_count.sum()
+        total_targets = cur_results.total_targets.sum()
+        on_target_rate = on_target / (on_target + off_target)
+
+        rtn += (
+            f"{os.path.basename(tbl)}"
+            + f"\t{';'.join(m)}"
+            + f"\t{min_ml}"
+            + f"\t{on_target_rate:.8}"
+            + f"\t{on_target/total_targets:.8}"
+            + f"\t{motif_at_count / at_count:.8}"
+            + f"\t{total_targets}"
+            + f"\t{fd.total_m6a_bp.sum()/fd.total_AT_bp.sum():.8}"
+            + f"\t{fd.total_m6a_bp.sum()}\n"
+        )
 
     sys.stderr.write(f"Done with {tbl} for {m}\n")
     return rtn
 
+
 def main():
     args = parse()
     print(
-        f"file\tmotif(s)\tm6A-within-motif(s)"
-        + "\tmotif(s)-covered-by-m6A\tAT-bases-covered-by-motif(s)\t#-of-motifs"
+        f"file\tmotifs\tmin_ml\tm6A-within-motifs"
+        + "\tmotifs-covered-by-m6A\tAT-bases-covered-by-motifs\tn-of-motifs"
         + "\tm6A-coverage"
-        + "\t#-of-m6A-bp"
+        + "\tn-of-m6A-bp"
     )
-    combos = [] 
+    combos = []
     for tbl in args.tbl:
         for m in ["All-AT-bp"] + [MOTIFS] + MOTIFS:
             if type(m) != list:
                 m = [m]
             combos.append((tbl, m))
-    #print(combos)
+    # print(combos)
 
     with Pool(args.t) as p:
         helper_by_table = partial(by_table, buffer=args.b, n_rows=args.n)
